8bac3552446850bfe0c9b4313cc26e5b5a6a5613
Cristofer Aliaga <aliagacristofer3@gmail.com>
2024-04-08 11:36:52 -0500

    add Factories

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Country;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Author>
+ */
+class AuthorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name' => fake()->name(),
+            'last_name' => fake()->lastName(),
+            'country_id' => Country::all()->random()->id
+        ];
+    }
+}
